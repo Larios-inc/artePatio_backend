95591cdaff3613a7cbf0ffad79499fdfec9430eb
David Castellanos <davcastellanoslarios@gmail.com>
2023-06-27 22:32:59 -0600

    working on product

--- src/controller/db-controllers/product-controllers/products.ts
+++ src/controller/db-controllers/product-controllers/products.ts
@@ -1,8 +1,84 @@
 import { Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from 'uuid';
 
+const { product } = new PrismaClient()
+
+interface DataProducts {
+    idProduct:    string
+    name_Product: string
+    price:        number
+    img_Main_Product:string
+    descriptionId:string
+    categoeryId:  string
+}
+
+export const createProduct = async( req:Request, res:Response, next:NextFunction ) => {
+
+    const {name_Product, price, img_Main_Product, descriptionId, categoeryId}:DataProducts = req.body
+
+    try {
+        
+        const id = uuidv4()
+
+        const postProduct: DataProducts = {
+            idProduct: id,
+            name_Product,
+            price,
+            img_Main_Product,
+            descriptionId,
+            categoeryId
+        }
+
+        await product.create({ data: postProduct})
+
+        res.status(201).json({
+            msg:'product posted succesfull :) ',
+            postProduct
+        })
+
+    } catch (error) {
+        next(error)
+    }
+
+}
 export const getAllProducts = async( req:Request, res:Response, next:NextFunction ) => {
 
-        res.status(200).json({
-            msg:'all products'
+
+
+}
+export const getByIdProduct = async( req:Request, res:Response, next:NextFunction ) => {
+
+    const { idProduct } = req.params
+
+    try {
+
+        const getProduct = await product.findUnique({
+            where:{
+                idProduct
+            },
+            include:{
+                orderItems: true,
+                monthlySale : true,
+                topSale: true
+            }
         })
+
+        res.status(200).json({
+            msg:'get one product',
+            getProduct
+        })
+        
+    } catch (error) {
+        next(error)
+    }
+
+}
+export const UpdateProduct = async( req:Request, res:Response, next:NextFunction ) => {
+
+
+}
+export const DeleteProduct = async( req:Request, res:Response, next:NextFunction ) => {
+
+
 }
--- src/routes/db-routes/routes-products/products.routes.ts
+++ src/routes/db-routes/routes-products/products.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
+import { check } from "express-validator";
 
-import { getAllProducts } from "../../../controller/db-controllers/product-controllers/products";
+import { getAllProducts,
+         DeleteProduct,
+         UpdateProduct,
+         createProduct,
+         getByIdProduct} from "../../../controller/db-controllers/product-controllers/products";
 
 const router = Router()
 
 router.get('/', getAllProducts )
+
+router.get('/:idProduct', getByIdProduct )
+
+router.post('/', createProduct )
+
+router.put('/:idProduct', UpdateProduct )
+
+router.delete('/:idProduct', DeleteProduct )
 
 export default router;
