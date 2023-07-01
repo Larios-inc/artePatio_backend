886b38584ebe929c48559bc387b15860ce3a73ba
David Castellanos <davcastellanoslarios@gmail.com>
2023-07-01 13:20:53 -0600

    working on address controller

--- src/controller/db-controllers/user-controllers/address.ts
+++ src/controller/db-controllers/user-controllers/address.ts
@@ -4,17 +4,11 @@ import { v4 as uuidv4 } from 'uuid';
 
 const {  } = new PrismaClient()
 
-interface DataAddress {
-    idAddress: string
-    address_line_1: string
-    address_line_2: string
-    address_line_3: string
-    ContactId: string
-}
 
-export const createAddress =async (req:Request, res: Response, next: NextFunction) => {}
-export const getByIdAddress =async (req:Request, res: Response, next: NextFunction) => {}
-export const updateAddress =async (req:Request, res: Response, next: NextFunction) => {}
-export const deleteAddress =async (raeq:Request, res: Response, next: NextFunction) => {}
-export const getAllAddresses =async (req:Request, res: Response, next: NextFunction) => {}
+
+export const createAddress = async (req:Request, res: Response, next: NextFunction) => {}
+export const getByIdAddress = async (req:Request, res: Response, next: NextFunction) => {}
+export const updateAddress = async (req:Request, res: Response, next: NextFunction) => {}
+export const deleteAddress = async (raeq:Request, res: Response, next: NextFunction) => {}
+export const getAllAddresses = async (req:Request, res: Response, next: NextFunction) => {}
   
