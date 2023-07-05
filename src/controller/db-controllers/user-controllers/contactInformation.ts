9a11373baa8822c3cf3c9547d625a921968b10cf
David Castellanos <davcastellanoslarios@gmail.com>
2023-07-04 21:05:55 -0600

    Update contactInformation.ts

--- src/controller/db-controllers/user-controllers/contactInformation.ts
+++ src/controller/db-controllers/user-controllers/contactInformation.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { Request, Response, NextFunction } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
-import { DataContactInfo } from '../../../ts/interfaces/products.interfaces';
+import { DataContactInfo } from '../../../ts/interfaces/user.interfaces';
 
 const {  } = new PrismaClient()
 
